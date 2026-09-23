90067ec9f5600718334e43914fd98482aa204346
raven-deadwire <memory_64085@naver.com>
2026-09-23 13:13:12 +0900

    Implement amp tone stack and presence resonance voicing

--- Source/dsp/LaneProcessor.cpp
+++ Source/dsp/LaneProcessor.cpp
@@ -1,6 +1,7 @@
 #include "LaneProcessor.h"
 #include <cmath>
 namespace spectralforge {
+void LaneProcessor::setTone(float b,float m,float t,float p,float r){bassDb=b;midDb=m;trebleDb=t;presenceDb=p;resonanceDb=r;*bassFilter.state=*juce::dsp::IIR::Coefficients<float>::makeLowShelf(sampleRate,90.f,.707f,juce::Decibels::decibelsToGain(b));*midFilter.state=*juce::dsp::IIR::Coefficients<float>::makePeakFilter(sampleRate,800.f,.75f,juce::Decibels::decibelsToGain(m));*trebleFilter.state=*juce::dsp::IIR::Coefficients<float>::makeHighShelf(sampleRate,3200.f,.707f,juce::Decibels::decibelsToGain(t));*presenceFilter.state=*juce::dsp::IIR::Coefficients<float>::makeHighShelf(sampleRate,2000.f,.8f,juce::Decibels::decibelsToGain(p));*resonanceFilter.state=*juce::dsp::IIR::Coefficients<float>::makePeakFilter(sampleRate,100.f,1.1f,juce::Decibels::decibelsToGain(r));}
 void LaneProcessor::process(juce::AudioBuffer<float>& b){
  if(muted){b.clear();return;} const float pg=1.f+drive*14.f;
  for(int n=0;n<b.getNumSamples();++n){ float g=level.getNextValue();
@@ -12,5 +13,5 @@ void LaneProcessor::process(juce::AudioBuffer<float>& b){
    p[n]=x*g;
   }
  }
- cab.process(b);
+ juce::dsp::AudioBlock<float> bl(b);juce::dsp::ProcessContextReplacing<float>ctx(bl);bassFilter.process(ctx);midFilter.process(ctx);trebleFilter.process(ctx);resonanceFilter.process(ctx);presenceFilter.process(ctx);cab.process(b);
 } }
