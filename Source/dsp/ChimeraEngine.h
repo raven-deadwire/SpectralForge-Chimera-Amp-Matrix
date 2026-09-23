2825837ae7b14d1602e9396405b1fa039abdf146
raven-deadwire <memory_64085@naver.com>
2026-09-23 13:06:35 +0900

    Fix DelayLine construction for JUCE build

--- Source/dsp/ChimeraEngine.h
+++ Source/dsp/ChimeraEngine.h
@@ -17,6 +17,7 @@ public:
 private:
  void applyAlignment(juce::AudioBuffer<float>&,int lane,const LaneSettings&);
  ThreeBandCrossover crossover; std::array<LaneProcessor,3> lanes; std::array<juce::AudioBuffer<float>,3> work;
- std::array<juce::dsp::DelayLine<float,juce::dsp::DelayLineInterpolationTypes::Linear>,3> delays{{ {256},{256},{256} }};
+ using Delay = juce::dsp::DelayLine<float,juce::dsp::DelayLineInterpolationTypes::Linear>;
+ std::array<Delay,3> delays { Delay(512), Delay(512), Delay(512) };
  double sampleRate{48000.0};
 };}
