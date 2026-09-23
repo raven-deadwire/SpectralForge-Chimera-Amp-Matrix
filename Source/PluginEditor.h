fe0147685f48ef3c895b4a5873384b628c219bbd
raven-deadwire <memory_64085@naver.com>
2026-09-23 12:59:24 +0900

    Use JUCE CMake module headers

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -1,5 +1,7 @@
 #pragma once
-#include <JuceHeader.h>
+#include <juce_audio_processors/juce_audio_processors.h>
+#include <juce_audio_utils/juce_audio_utils.h>
+#include <juce_dsp/juce_dsp.h>
 #include "PluginProcessor.h"
 class ChimeraAmpMatrixAudioProcessorEditor:public juce::AudioProcessorEditor{
 public:explicit ChimeraAmpMatrixAudioProcessorEditor(ChimeraAmpMatrixAudioProcessor&);void paint(juce::Graphics&)override;void resized()override;
