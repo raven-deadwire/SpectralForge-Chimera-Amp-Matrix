4af420ae3efe61f00c817e79790bd0b541fd0e61
raven-deadwire <memory_64085@naver.com>
2026-09-23 13:08:48 +0900

    Extend Pre FX rack with compressor and overdrive

--- Source/dsp/PreFxRack.h
+++ Source/dsp/PreFxRack.h
@@ -6,10 +6,10 @@ namespace spectralforge {
 class PreFxRack {
 public:
  void prepare(const juce::dsp::ProcessSpec&);void reset();
- void setGate(float thresholdDb,bool enabled);void setBoost(float gainDb,bool enabled);void setEq(float lowDb,float midDb,float highDb,bool enabled);
+ void setGate(float thresholdDb,bool enabled);void setCompressor(float thresholdDb,float ratio,float attackMs,float releaseMs,bool enabled);void setBoost(float gainDb,bool enabled);void setOverdrive(float drive,float tone,float levelDb,bool enabled);void setEq(float lowDb,float midDb,float highDb,bool enabled);
  void process(juce::AudioBuffer<float>&);
 private:
- double sampleRate{48000.0};float gateThreshold{0.001f},boostGain{1.f};bool gateOn{},boostOn{},eqOn{};
+ double sampleRate{48000.0};float gateThreshold{0.001f},boostGain{1.f},odDrive{0.f},odTone{0.5f},odLevel{1.f};bool gateOn{},compOn{},boostOn{},odOn{},eqOn{};juce::dsp::Compressor<float> compressor;juce::dsp::IIR::Filter<float> odToneFilter;
  juce::dsp::IIR::Filter<float> low,mid,high;
 };
 }
