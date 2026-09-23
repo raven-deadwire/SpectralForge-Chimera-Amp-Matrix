5165c6cfd60ad133ec385d7accb6dcfae6903925
raven-deadwire <memory_64085@naver.com>
2026-09-23 13:19:42 +0900

    Implement phase-matched complementary three-band LR4 crossover

--- Source/dsp/ThreeBandCrossover.cpp
+++ Source/dsp/ThreeBandCrossover.cpp
@@ -1,14 +1,11 @@
 #include "ThreeBandCrossover.h"
 namespace spectralforge {
-void ThreeBandCrossover::prepare(const juce::dsp::ProcessSpec&s){for(auto*f:{&lowLP,&midHP,&midLP,&highHP})f->prepare(s);setFrequencies(x1,x2);}
-void ThreeBandCrossover::reset(){for(auto*f:{&lowLP,&midHP,&midLP,&highHP})f->reset();}
-void ThreeBandCrossover::setFrequencies(float a,float b){x1=juce::jlimit(60.f,800.f,a);x2=juce::jlimit(x1+100.f,6000.f,b);
- lowLP.setType(F::Type::lowpass);midHP.setType(F::Type::highpass);midLP.setType(F::Type::lowpass);highHP.setType(F::Type::highpass);
- lowLP.setCutoffFrequency(x1);midHP.setCutoffFrequency(x1);midLP.setCutoffFrequency(x2);highHP.setCutoffFrequency(x2);}
+void ThreeBandCrossover::prepare(const juce::dsp::ProcessSpec&s){split1.prepare(s);split2.prepare(s);lowPhase.prepare(s);lowPhase.setType(juce::dsp::LinkwitzRileyFilterType::allpass);setFrequencies(x1,x2);}
+void ThreeBandCrossover::reset(){split1.reset();split2.reset();lowPhase.reset();}
+void ThreeBandCrossover::setFrequencies(float a,float b){x1=juce::jlimit(60.f,800.f,a);x2=juce::jlimit(x1+100.f,6000.f,b);split1.setCutoffFrequency(x1);split2.setCutoffFrequency(x2);lowPhase.setCutoffFrequency(x2);}
 void ThreeBandCrossover::split(const juce::AudioBuffer<float>&in,juce::AudioBuffer<float>&lo,juce::AudioBuffer<float>&mi,juce::AudioBuffer<float>&hi){
- lo.makeCopyOf(in,true);mi.makeCopyOf(in,true);hi.makeCopyOf(in,true);
- juce::dsp::AudioBlock<float> lb(lo),mb(mi),hb(hi);
- lowLP.process(juce::dsp::ProcessContextReplacing<float>(lb));
- midHP.process(juce::dsp::ProcessContextReplacing<float>(mb));midLP.process(juce::dsp::ProcessContextReplacing<float>(mb));
- highHP.process(juce::dsp::ProcessContextReplacing<float>(hb));
+ const int nc=in.getNumChannels(),ns=in.getNumSamples();lo.setSize(nc,ns,false,false,true);mi.setSize(nc,ns,false,false,true);hi.setSize(nc,ns,false,false,true);
+ for(int ch=0;ch<nc;++ch){auto*lp=lo.getWritePointer(ch);auto*mp=mi.getWritePointer(ch);auto*hp=hi.getWritePointer(ch);auto*src=in.getReadPointer(ch);
+  for(int n=0;n<ns;++n){float low{},upper{},mid{},high{};split1.processSample(ch,src[n],low,upper);split2.processSample(ch,upper,mid,high);lp[n]=lowPhase.processSample(ch,low);mp[n]=mid;hp[n]=high;}
+ }
 } }
