d21f4d4efb85efce679d90f6a22407fcace8f881
raven-deadwire <memory_64085@naver.com>
2026-09-23 13:18:29 +0900

    Use assignable IIR processors in PreFxRack.h

--- Source/dsp/PreFxRack.h
+++ Source/dsp/PreFxRack.h
@@ -9,7 +9,7 @@ public:
  void setGate(float thresholdDb,bool enabled);void setCompressor(float thresholdDb,float ratio,float attackMs,float releaseMs,bool enabled);void setBoost(float gainDb,bool enabled);void setOverdrive(float drive,float tone,float levelDb,bool enabled);void setEq(float lowDb,float midDb,float highDb,bool enabled);
  void process(juce::AudioBuffer<float>&);
 private:
- double sampleRate{48000.0};float gateThreshold{0.001f},boostGain{1.f},odDrive{0.f},odTone{0.5f},odLevel{1.f};bool gateOn{},compOn{},boostOn{},odOn{},eqOn{};juce::dsp::Compressor<float> compressor;juce::dsp::IIR::Filter<float> odToneFilter;
- juce::dsp::IIR::Filter<float> low,mid,high;
+ double sampleRate{48000.0};float gateThreshold{0.001f},boostGain{1.f},odDrive{0.f},odTone{0.5f},odLevel{1.f};bool gateOn{},compOn{},boostOn{},odOn{},eqOn{};juce::dsp::Compressor<float> compressor;juce::dsp::IIR::ProcessorDuplicator<juce::dsp::IIR::Filter<float>,juce::dsp::IIR::Coefficients<float>> odToneFilter;
+ juce::dsp::IIR::ProcessorDuplicator<juce::dsp::IIR::Filter<float>,juce::dsp::IIR::Coefficients<float>> low,mid,high;
 };
 }
