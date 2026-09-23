ae0428c7de2eca3855a9cb9a93f96d2f61397be6
raven-deadwire <memory_64085@naver.com>
2026-09-23 12:52:50 +0900

    Add Chimera MVP architecture and DSP primitives

---
+++ Source/dsp/ThreeBandCrossover.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <JuceHeader.h>
+namespace spectralforge {
+class ThreeBandCrossover {
+ using F=juce::dsp::LinkwitzRileyFilter<float>;
+public:
+ void prepare(const juce::dsp::ProcessSpec&); void reset(); void setFrequencies(float,float);
+ void split(const juce::AudioBuffer<float>&,juce::AudioBuffer<float>&,juce::AudioBuffer<float>&,juce::AudioBuffer<float>&);
+private: F lowLP,midHP,midLP,highHP; float x1{150.f},x2{1200.f};
+}; }
