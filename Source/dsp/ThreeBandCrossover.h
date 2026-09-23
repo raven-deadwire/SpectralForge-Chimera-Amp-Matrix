ac728c7c972ddf77a4dda88ec9c308b8a69e0c77
raven-deadwire <memory_64085@naver.com>
2026-09-23 12:59:33 +0900

    Use JUCE CMake module headers

--- Source/dsp/ThreeBandCrossover.h
+++ Source/dsp/ThreeBandCrossover.h
@@ -1,5 +1,7 @@
 #pragma once
-#include <JuceHeader.h>
+#include <juce_audio_processors/juce_audio_processors.h>
+#include <juce_audio_utils/juce_audio_utils.h>
+#include <juce_dsp/juce_dsp.h>
 namespace spectralforge {
 class ThreeBandCrossover {
  using F=juce::dsp::LinkwitzRileyFilter<float>;
