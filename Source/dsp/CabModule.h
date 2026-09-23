2ac900d8dc81bb360c4a58644b1e37c9eb3548ac
raven-deadwire <memory_64085@naver.com>
2026-09-23 13:10:52 +0900

    Add convolution IR loader interface

--- Source/dsp/CabModule.h
+++ Source/dsp/CabModule.h
@@ -4,6 +4,7 @@
 namespace spectralforge {
 class CabModule {
 public:
- void prepare(const juce::dsp::ProcessSpec&);void reset();void setEnabled(bool e){enabled=e;}void setLowCut(float hz);void setHighCut(float hz);void process(juce::AudioBuffer<float>&);
-private:double sampleRate{48000.0};bool enabled{true};float lowCutHz{70.f},highCutHz{9000.f};juce::dsp::IIR::Filter<float> hp,lp;
+ void prepare(const juce::dsp::ProcessSpec&);void reset();
+ bool loadImpulseResponse(const juce::File& file);void setEnabled(bool e){enabled=e;}void setLowCut(float hz);void setHighCut(float hz);void process(juce::AudioBuffer<float>&);
+private:double sampleRate{48000.0};bool enabled{true};float lowCutHz{70.f},highCutHz{9000.f};juce::dsp::IIR::Filter<float> hp,lp;juce::dsp::Convolution convolution;bool irLoaded{false};
 };}
