0f70848365860aed1a88f94375511ab3a386fcf0
raven-deadwire <memory_64085@naver.com>
2026-09-23 12:59:28 +0900

    Use JUCE CMake module headers

--- Source/dsp/ChimeraEngine.h
+++ Source/dsp/ChimeraEngine.h
@@ -1,5 +1,7 @@
 #pragma once
-#include <JuceHeader.h>
+#include <juce_audio_processors/juce_audio_processors.h>
+#include <juce_audio_utils/juce_audio_utils.h>
+#include <juce_dsp/juce_dsp.h>
 #include "ThreeBandCrossover.h"
 #include "LaneProcessor.h"
 namespace spectralforge {
