fa44aba79dd469d0606a88758f959966345a8659
raven-deadwire <memory_64085@naver.com>
2026-09-23 12:52:44 +0900

    Add Chimera MVP architecture and DSP primitives

---
+++ Source/dsp/LaneProcessor.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <JuceHeader.h>
+namespace spectralforge {
+enum class AmpModel : int { clean=0, tightDrive=1, bassSaturator=2 };
+class LaneProcessor {
+public:
+ void prepare(const juce::dsp::ProcessSpec& s){ level.reset(s.sampleRate,0.02); level.setCurrentAndTargetValue(1.0f); }
+ void reset(){}
+ void setModel(AmpModel m){ model=m; } void setDrive(float d){ drive=juce::jlimit(0.f,1.f,d); }
+ void setLevelDb(float d){ level.setTargetValue(juce::Decibels::decibelsToGain(d)); } void setMuted(bool m){ muted=m; }
+ void process(juce::AudioBuffer<float>&);
+private: AmpModel model{AmpModel::clean}; float drive{0.35f}; bool muted{}; juce::SmoothedValue<float> level{1.f};
+}; }
